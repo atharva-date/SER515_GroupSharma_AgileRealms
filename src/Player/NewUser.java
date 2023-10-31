8cca5f28a6310342d2154e7a3fd2549f2dfc5ec3
Milind Deshpande <mdeshp11@asu.edu>
2023-10-30 18:12:17 -0700

    Code restructuring for NewUser.java file

--- src/Player/NewUser.java
+++ src/Player/NewUser.java
@@ -1,9 +1,8 @@
 package Player;
-import Player.Login;
+//import Player.Login;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,10 +12,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextArea;
 import javax.swing.border.CompoundBorder;
-import java.awt.event.*;
 
 public class NewUser extends JFrame implements ActionListener {
 
@@ -35,9 +31,7 @@ public class NewUser extends JFrame implements ActionListener {
 		setBounds(100, 100, 1366, 1080);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 0, 0));
-
 		contentPane.setBorder(new CompoundBorder());
-
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
@@ -45,6 +39,13 @@ public class NewUser extends JFrame implements ActionListener {
 		panel.setBackground(new Color(0, 0, 0));
 		panel.setBounds(391, 40, 562, 600);
 		contentPane.add(panel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Create Account");
+		lblNewLabel_2.setBounds(93, 27, 347, 50);
+		lblNewLabel_2.setBackground(new Color(0, 0, 0));
+		lblNewLabel_2.setFont(new Font("Verdana", Font.BOLD, 40));
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		panel.add(lblNewLabel_2);
 				
 		JLabel lblFirstName = new JLabel("First Name");
 		lblFirstName.setForeground(Color.WHITE);
@@ -53,12 +54,22 @@ public class NewUser extends JFrame implements ActionListener {
 		lblFirstName.setBounds(104, 106, 103, 20);
 		panel.add(lblFirstName);
 		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(104, 137, 315, 34);
+		panel.add(textField_2);
+		
 		JLabel lblLastName = new JLabel("Last Name");
 		lblLastName.setForeground(Color.WHITE);
 		lblLastName.setFont(new Font("Verdana", Font.BOLD, 15));
 		lblLastName.setBackground(Color.WHITE);
 		lblLastName.setBounds(104, 200, 103, 20);
 		panel.add(lblLastName);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(104, 231, 315, 34);
+		panel.add(textField_1);
 		
 		JLabel lblNewLabel = new JLabel("Username");
 		lblNewLabel.setBounds(104, 292, 103, 20);
@@ -67,18 +78,20 @@ public class NewUser extends JFrame implements ActionListener {
 		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 15));
 		panel.add(lblNewLabel);
 		
+		textField = new JTextField();
+		textField.setBounds(104, 323, 315, 34);
+		panel.add(textField);
+		textField.setColumns(10);
+		
 		JLabel lblNewLabel_1 = new JLabel("Password");
 		lblNewLabel_1.setBounds(104, 384, 103, 20);
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
 		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 15));
 		panel.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Create Account");
-		lblNewLabel_2.setBounds(93, 27, 347, 50);
-		lblNewLabel_2.setBackground(new Color(0, 0, 0));
-		lblNewLabel_2.setFont(new Font("Verdana", Font.BOLD, 40));
-		lblNewLabel_2.setForeground(new Color(255, 255, 255));
-		panel.add(lblNewLabel_2);
+		passwordField = new JPasswordField();
+		passwordField.setBounds(104, 415, 315, 34);
+		panel.add(passwordField);
 				
 		Button button = new Button("Register");
 		button.setBounds(178, 503, 188, 43);
@@ -115,25 +128,6 @@ public class NewUser extends JFrame implements ActionListener {
 		button_1.setBackground(new Color(0, 0, 0));
 		button_1.setBounds(342, 580, 67, 21);
 		panel.add(button_1);
-		
-		textField = new JTextField();
-		textField.setBounds(104, 323, 315, 34);
-		panel.add(textField);
-		textField.setColumns(10);
-				
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(104, 231, 315, 34);
-		panel.add(textField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(104, 137, 315, 34);
-		panel.add(textField_2);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(104, 415, 315, 34);
-		panel.add(passwordField);
 	}
 
 @Override
