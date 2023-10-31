03a3101085bb62a7e6940b7e0b3bf32809eeb7e6
Milind Deshpande <mdeshp11@asu.edu>
2023-10-30 18:53:13 -0700

    Added QuillSword Font for both the Windows Login and Create Account

--- src/Player/Login.java
+++ src/Player/Login.java
@@ -1,6 +1,7 @@
 package Player;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,8 +10,12 @@ import java.awt.Button;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.FontFormatException;
+
 import javax.swing.JPasswordField;
 import java.awt.event.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Login extends JFrame {
 
@@ -19,6 +24,7 @@ public class Login extends JFrame {
 	private JFrame frame;
 	private JTextField textField;
 	private JPasswordField passwordField;
+	private JButton button_1, button;
 
 	/**
 	 * Launch the application.
@@ -42,6 +48,21 @@ public class Login extends JFrame {
 	public Login() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Font QuillSwordB = null;
+		Font QuillSwordB2 = null;
+		Font QuillSwordB3 = null;
+		try {
+			InputStream is = getClass().getResourceAsStream("../fonts/QuillSword-0Z24.otf");
+			InputStream is2 = getClass().getResourceAsStream("../fonts/QuillSword-0Z24.otf");
+			InputStream is3 = getClass().getResourceAsStream("../fonts/QuillSword-0Z24.otf");
+			QuillSwordB = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(Font.BOLD, 60);
+			QuillSwordB2 = Font.createFont(Font.TRUETYPE_FONT, is2).deriveFont(Font.BOLD, 25);
+			QuillSwordB3 = Font.createFont(Font.TRUETYPE_FONT, is3).deriveFont(Font.BOLD, 20);
+		} catch (FontFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		setBounds(100, 100, 1366, 1080);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 0, 0));
@@ -53,23 +74,23 @@ public class Login extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0, 0, 0));
-		panel.setBounds(391, 110, 533, 500);
+		panel.setBounds(391, 94, 533, 516);
 		panel.setAlignmentX(CENTER_ALIGNMENT);
 		panel.setAlignmentY(CENTER_ALIGNMENT);
 		contentPane.add(panel);
 		
 		JLabel lblNewLabel_2 = new JLabel("Login");
-		lblNewLabel_2.setBounds(102, 39, 135, 50);
+		lblNewLabel_2.setBounds(102, 11, 135, 78);
 		lblNewLabel_2.setBackground(new Color(0, 0, 0));
-		lblNewLabel_2.setFont(new Font("Verdana", Font.BOLD, 40));
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setFont(QuillSwordB);
 		panel.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel = new JLabel("Username");
-		lblNewLabel.setBounds(104, 142, 103, 20);
+		lblNewLabel.setBounds(104, 128, 103, 34);
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setBackground(new Color(255, 255, 255));
-		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 15));
+		lblNewLabel.setFont(QuillSwordB2);
 		panel.add(lblNewLabel);
 		
 		textField = new JTextField();
@@ -78,32 +99,35 @@ public class Login extends JFrame {
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Password");
-		lblNewLabel_1.setBounds(104, 237, 103, 20);
+		lblNewLabel_1.setBounds(102, 243, 103, 32);
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 15));
+		lblNewLabel_1.setFont(QuillSwordB2);
 		panel.add(lblNewLabel_1);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(104, 268, 315, 34);
+		passwordField.setBounds(104, 286, 315, 34);
 		panel.add(passwordField);
 		
-		Button button = new Button("Sign In");
+		button = new JButton("Sign In");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		button.setActionCommand("Sign In");
-		button.setBounds(176, 400, 188, 43);
-		button.setFont(new Font("Verdana", Font.BOLD, 15));
+		button.setBounds(190, 404, 141, 34);
 		button.setForeground(new Color(255, 255, 255));
-
 		panel.setLayout(null);
 		button.setBackground(new Color(192, 192, 192));
+		button.setFont(QuillSwordB3);
 		panel.add(button);
 		
 		JLabel lblNewLabel_3 = new JLabel("New to AgileRealms?");
-		lblNewLabel_3.setFont(new Font("Verdana", Font.BOLD, 15));
 		lblNewLabel_3.setForeground(new Color(255, 255, 255));
-		lblNewLabel_3.setBounds(141, 457, 181, 32);
+		lblNewLabel_3.setBounds(118, 463, 192, 32);
+		lblNewLabel_3.setFont(QuillSwordB2);
 		panel.add(lblNewLabel_3);
 		
-		Button button_1 = new Button("Register");
+		button_1 = new JButton("Register");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				EventQueue.invokeLater(new Runnable() {
@@ -119,10 +143,10 @@ public class Login extends JFrame {
 				});
 			}
 		});
-		button_1.setFont(new Font("Verdana", Font.BOLD, 15));
 		button_1.setBackground(new Color(0, 0, 0));
 		button_1.setForeground(new Color(0, 128, 255));
-		button_1.setBounds(332, 465, 80, 20);
+		button_1.setBounds(309, 465, 103, 25);
+		button_1.setFont(QuillSwordB3);
 		panel.add(button_1);
 	}
 }
