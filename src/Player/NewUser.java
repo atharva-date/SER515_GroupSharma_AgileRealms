7f1695e9e81966363a3d3b46213b34ab6ea4bfa1
Milind Deshpande <mdeshp11@asu.edu>
2023-11-01 23:00:29 -0700

    Added the Layered Pane implementation for Create Account and Login UI windows

--- src/Player/NewUser.java
+++ src/Player/NewUser.java
@@ -16,12 +16,14 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.CompoundBorder;
 import java.awt.event.*;
+import javax.swing.JLayeredPane;
+import java.awt.CardLayout;
+import javax.swing.SwingConstants;
 
 public class NewUser extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JPanel panel;
+	private JPanel contentPane, createAccPanel, loginPanel;
 	private JPasswordField passwordField;
 	private JTextField textField;
 	private JTextField textField_1;
@@ -57,25 +59,39 @@ public class NewUser extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		panel = new JPanel();
-		panel.setBackground(new Color(0, 0, 0));
-		panel.setBounds(391, 42, 562, 620);
-		contentPane.add(panel);
+		// Layered Panel for New User Account creation and Login Page
 		
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(380, 30, 550, 580);
+		contentPane.add(layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+				
+		// Create Account Panel
+		
+		createAccPanel = new JPanel();
+		createAccPanel.setBackground(new Color(0, 0, 0));
+		layeredPane.add(createAccPanel, "name_361890388473000");
+		createAccPanel.setLayout(null);
+				
 		JLabel lblNewLabel_2 = new JLabel("Create Account");
 		lblNewLabel_2.setBounds(93, 11, 347, 68);
 		lblNewLabel_2.setBackground(new Color(0, 0, 0));
 		lblNewLabel_2.setForeground(new Color(255, 255, 255));
-		panel.add(lblNewLabel_2);
+		createAccPanel.add(lblNewLabel_2);
 		lblNewLabel_2.setFont(QuillSwordB);
-				
+		
 		JLabel lblFirstName = new JLabel("First Name");
 		lblFirstName.setForeground(Color.WHITE);
 		lblFirstName.setFont(new Font("Verdana", Font.BOLD, 15));
 		lblFirstName.setBackground(Color.WHITE);
 		lblFirstName.setBounds(104, 92, 103, 34);
 		lblFirstName.setFont(QuillSwordB2);
-		panel.add(lblFirstName);
+		createAccPanel.add(lblFirstName);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(104, 137, 315, 34);
+		createAccPanel.add(textField_2);
 		
 		JLabel lblLastName = new JLabel("Last Name");
 		lblLastName.setForeground(Color.WHITE);
@@ -83,7 +99,12 @@ public class NewUser extends JFrame implements ActionListener {
 		lblLastName.setBackground(Color.WHITE);
 		lblLastName.setBounds(104, 193, 103, 27);
 		lblLastName.setFont(QuillSwordB2);
-		panel.add(lblLastName);
+		createAccPanel.add(lblLastName);
+				
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(104, 231, 315, 34);
+		createAccPanel.add(textField_1);
 		
 		JLabel lblNewLabel = new JLabel("Username");
 		lblNewLabel.setBounds(104, 285, 103, 27);
@@ -91,75 +112,129 @@ public class NewUser extends JFrame implements ActionListener {
 		lblNewLabel.setBackground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 15));
 		lblNewLabel.setFont(QuillSwordB2);
-		panel.add(lblNewLabel);
+		createAccPanel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(104, 323, 315, 34);
+		createAccPanel.add(textField);
+		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Password");
 		lblNewLabel_1.setBounds(104, 377, 103, 27);
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
 		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 15));
 		lblNewLabel_1.setFont(QuillSwordB2);
-		panel.add(lblNewLabel_1);
-				
+		createAccPanel.add(lblNewLabel_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(104, 415, 315, 34);
+		createAccPanel.add(passwordField);
+		
 		button = new JButton("Register");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		button.setBounds(201, 486, 148, 34);
+		createAccPanel.setLayout(null);
+		button.setForeground(new Color(255, 255, 255));
+		button.setBackground(new Color(192, 192, 192));
+		button.setFont(QuillSwordB3);
+		createAccPanel.add(button);
+		
+		JLabel lblNewLabel_3 = new JLabel("Already Registered?");
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setBounds(150, 535, 179, 45);
+		lblNewLabel_3.setFont(QuillSwordB2);
+		createAccPanel.add(lblNewLabel_3);
+		
+		button = new JButton("Login");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				layeredPane.removeAll();
+				layeredPane.add(loginPanel);
+				layeredPane.repaint();
+				layeredPane.revalidate();
+			}
+		});
+		button.setBackground(new Color(0, 0, 0));
+		button.setForeground(new Color(255, 255, 255));
+		button.setBounds(330, 542, 80, 25);
+		button.setFont(QuillSwordB3);
+		createAccPanel.add(button);
+		
+		// Login Panel
+		
+		loginPanel = new JPanel();
+		loginPanel.setBackground(new Color(0, 0, 0));
+		layeredPane.add(loginPanel, "name_361630997658600");
+		loginPanel.setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("Login");
+		lblNewLabel_4.setBounds(102, 11, 135, 78);
+		lblNewLabel_4.setBackground(new Color(0, 0, 0));
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setFont(QuillSwordB);
+		loginPanel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Username");
+		lblNewLabel_5.setBounds(104, 128, 103, 34);
+		lblNewLabel_5.setForeground(new Color(255, 255, 255));
+		lblNewLabel_5.setBackground(new Color(255, 255, 255));
+		lblNewLabel_5.setFont(QuillSwordB2);
+		loginPanel.add(lblNewLabel_5);
+		
+		textField = new JTextField();
+		textField.setBounds(104, 173, 315, 34);
+		loginPanel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Password");
+		lblNewLabel_6.setBounds(102, 243, 103, 32);
+		lblNewLabel_6.setForeground(new Color(255, 255, 255));
+		lblNewLabel_6.setFont(QuillSwordB2);
+		loginPanel.add(lblNewLabel_6);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(104, 286, 315, 34);
+		loginPanel.add(passwordField);
+		
+		button = new JButton("Sign In");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		button.setBounds(201, 513, 148, 34);
-		button.setFont(new Font("Verdana", Font.BOLD, 15));
-		panel.setLayout(null);
+		button.setActionCommand("Sign In");
+		button.setBounds(190, 404, 141, 34);
 		button.setForeground(new Color(255, 255, 255));
+		loginPanel.setLayout(null);
 		button.setBackground(new Color(192, 192, 192));
 		button.setFont(QuillSwordB3);
-		panel.add(button);
+		loginPanel.add(button);
 		
-		JLabel lblNewLabel_3 = new JLabel("Already Registered?");
-		lblNewLabel_3.setFont(new Font("Verdana", Font.BOLD, 15));
-		lblNewLabel_3.setForeground(new Color(255, 255, 255));
-		lblNewLabel_3.setBounds(144, 572, 173, 27);
-		lblNewLabel_3.setFont(QuillSwordB2);
-		panel.add(lblNewLabel_3);
-				
-		button_1 = new JButton("Login");
+		JLabel lblNewLabel_7 = new JLabel("New to AgileRealms?");
+		lblNewLabel_7.setForeground(new Color(255, 255, 255));
+		lblNewLabel_7.setBounds(118, 470, 192, 32);
+		lblNewLabel_7.setFont(QuillSwordB2);
+		loginPanel.add(lblNewLabel_7);
+		
+		button_1 = new JButton("Register");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				EventQueue.invokeLater(new Runnable() {
-					public void run(){
-						try {
-							Login frame = new Login();
-							frame.setTitle("User Login");
-							frame.setVisible(true);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				});
+				// TODO Auto-generated method stub
+				layeredPane.removeAll();
+				layeredPane.add(createAccPanel);
+				layeredPane.repaint();
+				layeredPane.revalidate();
 			}
 		});
-		button_1.setForeground(new Color(0, 128, 255));
 		button_1.setBackground(new Color(0, 0, 0));
-		button_1.setBounds(327, 572, 77, 22);
+		button_1.setForeground(new Color(255, 255, 255));
+		button_1.setBounds(309, 470, 95, 25);
 		button_1.setFont(QuillSwordB3);
-		panel.add(button_1);
-		
-		textField = new JTextField();
-		textField.setBounds(104, 323, 315, 34);
-		panel.add(textField);
-		textField.setColumns(10);
-				
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(104, 231, 315, 34);
-		panel.add(textField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(104, 137, 315, 34);
-		panel.add(textField_2);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(104, 415, 315, 34);
-		panel.add(passwordField);
+		loginPanel.add(button_1);
 	}
 
 @Override
